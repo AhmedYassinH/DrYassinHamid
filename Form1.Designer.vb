e89df1729a831b219c2591cdb8de347eaaac6bf5
Ahmed Yassin <71176586+AhmedYassinH@users.noreply.github.com>
2022-05-07 15:18:36 +0300

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,376 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtphone = New System.Windows.Forms.TextBox()
+        Me.txtinf = New System.Windows.Forms.TextBox()
+        Me.txtm = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.dtp = New System.Windows.Forms.DateTimePicker()
+        Me.PictureBox8 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox7 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PrintDocument1 = New System.Drawing.Printing.PrintDocument()
+        Me.PrintDialog1 = New System.Windows.Forms.PrintDialog()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.lbl_Date = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Andalus", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Black
+        Me.Label1.Location = New System.Drawing.Point(679, 43)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(123, 41)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = ": رقم المريض"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Andalus", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Black
+        Me.Label2.Location = New System.Drawing.Point(266, 43)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(133, 41)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = ": اسم المريض "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Andalus", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Black
+        Me.Label3.Location = New System.Drawing.Point(679, 101)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(81, 41)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = ": التاريخ"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Andalus", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Black
+        Me.Label4.Location = New System.Drawing.Point(266, 101)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(120, 41)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = ": رقم الهاتف"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Andalus", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.Black
+        Me.Label5.Location = New System.Drawing.Point(714, 280)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(103, 41)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = ": التشخيص"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Andalus", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.Black
+        Me.Label6.Location = New System.Drawing.Point(335, 280)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(80, 41)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = " : العلاج"
+        '
+        'txtID
+        '
+        Me.txtID.Location = New System.Drawing.Point(468, 59)
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(188, 20)
+        Me.txtID.TabIndex = 7
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(32, 59)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(216, 20)
+        Me.txtName.TabIndex = 8
+        '
+        'txtphone
+        '
+        Me.txtphone.Location = New System.Drawing.Point(32, 117)
+        Me.txtphone.Name = "txtphone"
+        Me.txtphone.Size = New System.Drawing.Size(216, 20)
+        Me.txtphone.TabIndex = 10
+        '
+        'txtinf
+        '
+        Me.txtinf.Location = New System.Drawing.Point(412, 171)
+        Me.txtinf.Multiline = True
+        Me.txtinf.Name = "txtinf"
+        Me.txtinf.Size = New System.Drawing.Size(296, 268)
+        Me.txtinf.TabIndex = 11
+        '
+        'txtm
+        '
+        Me.txtm.Location = New System.Drawing.Point(16, 171)
+        Me.txtm.Multiline = True
+        Me.txtm.Name = "txtm"
+        Me.txtm.Size = New System.Drawing.Size(324, 268)
+        Me.txtm.TabIndex = 12
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.dtp)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.txtm)
+        Me.GroupBox1.Controls.Add(Me.txtID)
+        Me.GroupBox1.Controls.Add(Me.txtinf)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtphone)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.txtName)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 121)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(832, 455)
+        Me.GroupBox1.TabIndex = 13
+        Me.GroupBox1.TabStop = False
+        '
+        'dtp
+        '
+        Me.dtp.Location = New System.Drawing.Point(435, 114)
+        Me.dtp.Name = "dtp"
+        Me.dtp.Size = New System.Drawing.Size(221, 20)
+        Me.dtp.TabIndex = 18
+        '
+        'PictureBox8
+        '
+        Me.PictureBox8.Image = Global.DrYassinHamid.My.Resources.Resources.Tech_Update
+        Me.PictureBox8.Location = New System.Drawing.Point(568, 600)
+        Me.PictureBox8.Name = "PictureBox8"
+        Me.PictureBox8.Size = New System.Drawing.Size(100, 53)
+        Me.PictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox8.TabIndex = 26
+        Me.PictureBox8.TabStop = False
+        '
+        'PictureBox7
+        '
+        Me.PictureBox7.Image = Global.DrYassinHamid.My.Resources.Resources.clearhistory
+        Me.PictureBox7.Location = New System.Drawing.Point(191, 602)
+        Me.PictureBox7.Name = "PictureBox7"
+        Me.PictureBox7.Size = New System.Drawing.Size(100, 53)
+        Me.PictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox7.TabIndex = 25
+        Me.PictureBox7.TabStop = False
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.Image = Global.DrYassinHamid.My.Resources.Resources.sq6thv5y6ljc0sabbhnv
+        Me.PictureBox6.Location = New System.Drawing.Point(85, 602)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(100, 53)
+        Me.PictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox6.TabIndex = 24
+        Me.PictureBox6.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.Image = Global.DrYassinHamid.My.Resources.Resources.button_cancel
+        Me.PictureBox5.Location = New System.Drawing.Point(486, 591)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(76, 62)
+        Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox5.TabIndex = 23
+        Me.PictureBox5.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = Global.DrYassinHamid.My.Resources.Resources.images
+        Me.PictureBox4.Location = New System.Drawing.Point(380, 600)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(100, 53)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 22
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.DrYassinHamid.My.Resources.Resources.Save
+        Me.PictureBox3.Location = New System.Drawing.Point(674, 582)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(76, 71)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 21
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.DrYassinHamid.My.Resources.Resources.fsfs
+        Me.PictureBox2.Location = New System.Drawing.Point(297, 600)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(77, 55)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 20
+        Me.PictureBox2.TabStop = False
+        '
+        'PrintDocument1
+        '
+        '
+        'PrintDialog1
+        '
+        Me.PrintDialog1.Document = Me.PrintDocument1
+        Me.PrintDialog1.UseEXDialog = True
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Andalus", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.ForeColor = System.Drawing.Color.Red
+        Me.Label7.Location = New System.Drawing.Point(275, 58)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(263, 57)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "تسجيل بيانات المرضى"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.DrYassinHamid.My.Resources.Resources.تنزيل_Yassin_Hamid
+        Me.PictureBox1.Location = New System.Drawing.Point(590, 9)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(224, 106)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 14
+        Me.PictureBox1.TabStop = False
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Edwardian Script ITC", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Label8.Location = New System.Drawing.Point(5, 22)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(235, 41)
+        Me.Label8.TabIndex = 15
+        Me.Label8.Text = "Dr/YassinHamid"
+        '
+        'lbl_Date
+        '
+        Me.lbl_Date.Font = New System.Drawing.Font("Tekton Pro Ext", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_Date.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.lbl_Date.Location = New System.Drawing.Point(12, 103)
+        Me.lbl_Date.Name = "lbl_Date"
+        Me.lbl_Date.Size = New System.Drawing.Size(233, 70)
+        Me.lbl_Date.TabIndex = 16
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.ClientSize = New System.Drawing.Size(848, 664)
+        Me.Controls.Add(Me.PictureBox6)
+        Me.Controls.Add(Me.PictureBox7)
+        Me.Controls.Add(Me.PictureBox8)
+        Me.Controls.Add(Me.lbl_Date)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "DrYassinHamid"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents txtID As System.Windows.Forms.TextBox
+    Friend WithEvents txtName As System.Windows.Forms.TextBox
+    Friend WithEvents txtphone As System.Windows.Forms.TextBox
+    Friend WithEvents txtinf As System.Windows.Forms.TextBox
+    Friend WithEvents txtm As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents dtp As System.Windows.Forms.DateTimePicker
+    Friend WithEvents PrintDocument1 As System.Drawing.Printing.PrintDocument
+    Friend WithEvents PrintDialog1 As System.Windows.Forms.PrintDialog
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox4 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox5 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox6 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox7 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox8 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents lbl_Date As System.Windows.Forms.Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,287 @@
+﻿Imports System.Data.OleDb
+
+Public Class Form1
+    Dim con As New OleDbConnection
+    
+
+    Private Sub TextBox5_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtinf.TextChanged
+
+    End Sub
+
+    Private Sub Label3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        End
+    End Sub
+
+    Private Sub TextBox4_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtphone.TextChanged
+
+    End Sub
+
+    Private Sub GroupBox1_Enter(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+       
+
+
+
+        con.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + Application.StartupPath + "\DrYassinHamid.accdb"
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        con.Open()
+        Try
+            Dim cmdupdate As New OleDbCommand
+            cmdupdate.Connection = con
+            cmdupdate.CommandText = "Update DrYassinHamid set m_Name = '" + txtName.Text + "' , m_Date = '" + dtp.Value + "' , m_phone = '" + txtphone.Text + " ' , m_inf = '" + txtinf.Text + " ',m_m='" + txtm.Text + "' Where m_ID = '" + txtID.Text + "'"
+            cmdupdate.ExecuteNonQuery()
+            MsgBox("تم تعديل بيانات المريض")
+            txtID.Clear()
+            txtName.Clear()
+            txtinf.Clear()
+            txtphone.Clear()
+            txtm.Clear()
+            dtp.Value = Now
+        Catch ex As Exception
+            MsgBox(ex.Message.ToString)
+        End Try
+        con.Close()
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        con.Open()
+        Try
+            Dim cmddelet As New OleDbCommand
+            cmddelet.Connection = con
+            cmddelet.CommandText = "Delete From DrYassinHamid where m_ID = '" + txtID.Text + "'"
+            cmddelet.ExecuteNonQuery()
+            MsgBox("تم حذف المريض")
+            txtID.Clear()
+            txtName.Clear()
+            txtinf.Clear()
+            txtphone.Clear()
+            txtm.Clear()
+            dtp.Value = Now
+        Catch ex As Exception
+            MsgBox(ex.Message.ToString)
+        End Try
+        con.Close()
+    End Sub
+
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        txtID.Clear()
+        txtName.Clear()
+        txtinf.Clear()
+        txtphone.Clear()
+        txtm.Clear()
+        dtp.Value = Now
+
+    End Sub
+
+    Private Sub PrintDocument1_PrintPage(sender As Object, e As Printing.PrintPageEventArgs) Handles PrintDocument1.PrintPage
+        e.Graphics.DrawString(txtm.Text, New Font("Tahoma", 20), Brushes.Black, 10, 20)
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+
+    Private Sub Label9_Click_1(sender As Object, e As EventArgs) Handles lbl_Date.Click
+        lbl_Date.Text = Now
+    End Sub
+
+    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Label7.Click
+
+    End Sub
+
+    Private Sub txtID_TextChanged(sender As Object, e As EventArgs) Handles txtID.TextChanged
+
+    End Sub
+
+    Private Sub lbl_Date_TextChanged(sender As Object, e As EventArgs) Handles lbl_Date.TextChanged
+
+    End Sub
+
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+
+        
+
+
+
+
+
+        If PrintDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+
+            PrintDocument1.Print()
+
+        End If
+    End Sub
+
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
+
+
+      
+
+
+        con.Open()
+        Try
+            Dim cmdinsert As New OleDbCommand
+            cmdinsert.Connection = con
+            cmdinsert.CommandText = "insert into DrYassinHamid values ('" + txtID.Text + "','" + txtName.Text + "','" + dtp.Value + "','" + txtphone.Text + "','" + txtinf.Text + "','" + txtm.Text + "')"
+            cmdinsert.ExecuteNonQuery()
+            MsgBox("تم حفظ المريض")
+            txtID.Clear()
+            txtName.Clear()
+            txtinf.Clear()
+            txtphone.Clear()
+            txtm.Clear()
+            dtp.Value = Now
+
+
+        Catch ex As Exception
+            MsgBox("عفوا هذا المريض مسجل مسبقا".ToString)
+        End Try
+        con.Close()
+    End Sub
+
+    Private Sub PictureBox4_Click(sender As Object, e As EventArgs) Handles PictureBox4.Click
+
+
+
+
+
+
+
+        con.Open()
+        Try
+            Dim cmdselect As New OleDbCommand
+            cmdselect.Connection = con
+            cmdselect.CommandText = " select * from DrYassinHamid Where m_ID = '" + txtID.Text + "' or m_phone = '" + txtphone.Text + "' or m_inf = '" + txtinf.Text + "'"
+            Dim re As OleDbDataReader
+            re = cmdselect.ExecuteReader
+            If re.Read Then
+                txtID.Text = re(0).ToString
+                txtName.Text = re(1).ToString
+                dtp.Value = re(2).ToString
+                txtphone.Text = re(3).ToString
+                txtinf.Text = re(4).ToString
+                txtm.Text = re(5).ToString
+
+            Else
+                MsgBox("عفوا هذا المريض غير مسجل")
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.Message.ToString)
+        End Try
+        con.Close()
+    End Sub
+
+    Private Sub PictureBox5_Click(sender As Object, e As EventArgs) Handles PictureBox5.Click
+
+       
+
+
+
+
+        con.Open()
+        Try
+            Dim cmddelet As New OleDbCommand
+            cmddelet.Connection = con
+            cmddelet.CommandText = "Delete From DrYassinHamid where m_ID = '" + txtID.Text + "'"
+            cmddelet.ExecuteNonQuery()
+            MsgBox("تم حذف المريض")
+            txtID.Clear()
+            txtName.Clear()
+            txtinf.Clear()
+            txtphone.Clear()
+            txtm.Clear()
+            dtp.Value = Now
+        Catch ex As Exception
+            MsgBox(ex.Message.ToString)
+        End Try
+        con.Close()
+
+    End Sub
+
+    Private Sub PictureBox6_Click(sender As Object, e As EventArgs) Handles PictureBox6.Click
+       
+
+
+
+
+
+        End
+
+
+
+        
+
+
+
+
+    End Sub
+
+    Private Sub PictureBox7_Click(sender As Object, e As EventArgs) Handles PictureBox7.Click
+
+     
+
+        txtID.Clear()
+        txtName.Clear()
+        txtinf.Clear()
+        txtphone.Clear()
+        txtm.Clear()
+        dtp.Value = Now
+    End Sub
+
+    Private Sub PictureBox8_Click(sender As Object, e As EventArgs) Handles PictureBox8.Click
+
+
+      
+
+
+
+
+
+        con.Open()
+        Try
+            Dim cmdupdate As New OleDbCommand
+            cmdupdate.Connection = con
+            cmdupdate.CommandText = "Update DrYassinHamid set m_Name = '" + txtName.Text + "' , m_Date = '" + dtp.Value + "' , m_phone = '" + txtphone.Text + " ' , m_inf = '" + txtinf.Text + " ',m_m='" + txtm.Text + "' Where m_ID = '" + txtID.Text + "'"
+            cmdupdate.ExecuteNonQuery()
+            MsgBox("تم تعديل بيانات المريض")
+            txtID.Clear()
+            txtName.Clear()
+            txtinf.Clear()
+            txtphone.Clear()
+            txtm.Clear()
+            dtp.Value = Now
+        Catch ex As Exception
+            MsgBox(ex.Message.ToString)
+        End Try
+        con.Close()
+    End Sub
+
+    Private Sub PictureBox3_MouseMove(sender As Object, e As MouseEventArgs) Handles PictureBox3.MouseMove
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        End
+    End Sub
+
+    Private Sub txtm_TextChanged(sender As Object, e As EventArgs) Handles txtm.TextChanged
+
+    End Sub
+
+    Private Sub Label9_Click(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
